c29e6d0ee313e26b29e9340732b87492bcdeac9a
licolsan <phamhoanglinh198@gmail.com>
2017-12-21 11:28:42 +0700

    Change host (remove '/login')

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://our-change-kins.herokuapp.com/login'
+  host = 'https://our-change-kins.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
