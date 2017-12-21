a0b3517a527ab40872d0c33fe6b37eaa93add6f7
licolsan <phamhoanglinh198@gmail.com>
2017-12-21 10:27:48 +0700

    Change host to https://our-change-kins.herokuapp.com/

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000'
+  host = 'https://our-change-kins.herokuapp.com/login'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
